76a119eaf937495fc8537dbfde22bac854a9fa8f
Fernando Dias Motta <fernando3dias@gmail.com>
2023-10-16 14:21:02 -0300

    feat: client created

--- gRPCClient/Program.cs
+++ gRPCClient/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+
+var channel = GrpcChannel.ForAddress("https://localhost:7136");
+var client = new TaskTracker.TaskTrackerClient(channel);
+
+var createResponse = await client.CreateTaskAsync(
+    new CreateTaskRequest
+    {
+        Title = "Minha tarefa 01",
+        Content = "Uma descrição qualquer",
+        Tag = TaskPriority.TpCommon
+    });
+
+Console.WriteLine($"Tarefa criada com o ID: {createResponse.TaskId}");
+
+// Feche o canal gRPC quando não for mais necessário
+channel.ShutdownAsync().Wait();
+
+Console.WriteLine("Pressione qualquer tecla para sair...");
+Console.ReadKey();
