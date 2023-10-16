dc0bd97692f67d5f8a00efe66c5afb19db319b9c
Fernando Dias Motta <fernando3dias@gmail.com>
2023-10-16 15:49:01 -0300

    updating

--- gRPCClient/Program.cs
+++ gRPCClient/Program.cs
@@ -7,6 +7,53 @@ using System.Threading.Tasks;
 var channel = GrpcChannel.ForAddress("https://localhost:7136");
 var client = new TaskTracker.TaskTrackerClient(channel);
 
+
+Menu();
+static void Menu() {
+    string option = "";
+    Console.WriteLine("-----------------------------------------");
+    Console.WriteLine("Seja bem ao seu gerenciador de Tarefas!");
+    Console.WriteLine("-----------------------------------------");
+    Console.WriteLine("Aperte uma opção para prosseguir:\n");
+    Console.WriteLine("1: Criar uma nova tarefa");
+    Console.WriteLine("2: Listar todas tarefas");
+    Console.WriteLine("3: Executar uma tarefa");
+    Console.WriteLine("4: Remover uma tarefa`\n");
+    Console.Write("Digite sua opção: ");
+    option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+            Console.WriteLine("Opção 1 selecionada: Criar uma nova tarefa");
+            // Adicione aqui o código para criar uma nova tarefa
+            break;
+        case "2":
+            Console.WriteLine("Opção 2 selecionada: Listar todas as tarefas");
+            // Adicione aqui o código para listar todas as tarefas
+            break;
+        case "3":
+            Console.WriteLine("Opção 3 selecionada: Executar uma tarefa");
+            // Adicione aqui o código para executar uma tarefa
+            break;
+        case "4":
+            Console.WriteLine("Opção 4 selecionada: Remover uma tarefa");
+            // Adicione aqui o código para remover uma tarefa
+            break;
+        case "5":
+            Console.WriteLine("Saindo do programa. Até logo!");
+            return;
+        default:
+            Console.WriteLine("Opção inválida. Por favor, escolha uma opção válida.");
+            break;
+    }
+}
+
+
+
+
+
+
 var createResponse = await client.CreateTaskAsync(
     new CreateTaskRequest
     {
